f59d4217af9683ff57e2b95daf136f7eab0c0364
Kim Sun <kim.sun@ericsson.com>
2018-08-30 09:28:32 +0800

    some refine

--- day01/main.go
+++ day01/main.go
@@ -1,11 +1,13 @@
 package main
 
+/*
 func servcies() {
 	services := []string{"udm-01", "udm-02"}
 	for _, service := range services {
 		//service-names=srv11&service-names=srv21
 	}
 }
+*/
 
 func main() {
 	sliceTest()
